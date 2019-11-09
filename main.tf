b15fb121a3cbd170acefcc724aa475eedb64b36f
Rohan <rohan@imentor.co.in>
2019-11-09 14:43:12 +0530

    asd

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "32c32163-f026-41ed-9503-3b0ee6ec5540"
+  client_id       = "b9543670-34cb-410d-ad75-fb29e77964cc"
+  client_secret   = "Jg-xsz_p7Ru/5TOiRke_F1fsimnVUVJ8"
+  tenant_id       = "f66ccb58-5436-40de-ae8b-e951e03cb2b3"
+}
